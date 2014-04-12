9de0bdece1fa333ec08dd4f17baac8ba6c74ad3e
MindlessLump <ewok.attok@gmail.com>
2014-04-12 14:39:17 -0600

    Working on the revamp

--- Item.java
+++ Item.java
@@ -1,5 +1,84 @@
 package textGame;
 
 public class Item {
-
+	private String placeDescription; //Description of where the item is found
+	private String itemDescription; //Description of the item itself
+	private String onPickup; //Contains a command to be carried out when the item is picked up
+	private String onUse; //Contains a command to be carried out when the item is used
+	private boolean infinite; //Whether the item can be used an infinite number of times
+	private int itemUses;
+	
+	//Empty constructor
+	public Item() {
+		
+	}
+	
+	//Full constructor
+	public Item(String place, String item) {
+		placeDescription = place;
+		itemDescription = item;
+	}
+	
+	//Set the place description
+	public void setPlaceDescription(String place) {
+		placeDescription = place;
+	}
+	
+	//Return the place description
+	public String getPlaceDescription() {
+		return placeDescription;
+	}
+	
+	//Set the item description
+	public void setItemDescription(String item) {
+		itemDescription = item;
+	}
+	
+	//Return the item description
+	public String getItemDescription() {
+		return itemDescription;
+	}
+	
+	//Set the item to be infinite
+	public void setInfiniteUses() {
+		infinite = true;
+	}
+	
+	//Set the item to be finite
+	public void setFiniteUses(int uses) {
+		infinite = false;
+		itemUses = uses;
+	}
+	
+	//Return the number of uses
+	public int getUses() {
+		return itemUses;
+	}
+	
+	//Return whether the item is infinite
+	public boolean isInfinite() {
+		return infinite;
+	}
+	
+	//Set the onPickup command
+	public void setOnPickup(String pick) {
+		onPickup = pick;
+	}
+	
+	//Perform the onPickup command
+	public void doPickup() {
+		Background.doCommand(onPickup);
+	}
+	
+	//Set the onUse command
+	public void setOnUse(String use) {
+		onUse = use;
+	}
+	
+	//Perform the onUse command
+	public void doUse() {
+		Background.doCommand(onUse);
+		if(!this.isInfinite())
+			itemUses--;
+	}
 }
--- Player.java
+++ Player.java
@@ -1,5 +1,39 @@
 package textGame;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+	private Location currentLocation = new Location();
+	private ArrayList<Item> inventory = new ArrayList<Item>();
+	
+	//Empty constructor
+	public Player() {
+		
+	}
+	
+	//Set the current location
+	public void setCurrentLocation(Location loc) {
+		currentLocation = loc;
+	}
+	
+	//Return the current location
+	public Location getCurrentLocation() {
+		return currentLocation;
+	}
+	
+	//Add an item to the inventory
+	public void addItem(Item i) {
+		inventory.add(i);
+	}
+	
+	//Remove an item from the inventory
+	public void removeItem(Item i) {
+		if(inventory.contains(i))
+			inventory.remove(i);
+	}
+	
+	//Return the list of items
+	public ArrayList<Item> getInventory() {
+		return inventory;
+	}
 }
